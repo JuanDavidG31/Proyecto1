1edeebd3a46220031fa5845adeaaa6b0a752aceb
juand <juand@PCJUAN>
2024-09-12 18:44:46 -0500

    Creacion model y view en controller

--- PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/controller/Controller.java
+++ PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/controller/Controller.java
@@ -1,8 +1,14 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.model.ModelFacade;
+import co.edu.unbosque.view.ViewFacade;
+
 public class Controller {
+	private ModelFacade mf;
+	private ViewFacade vf;
 	public Controller() {
-		// TODO Auto-generated constructor stub
+		mf = new ModelFacade();
+		vf = new ViewFacade();
 	}
 	public void run() {
 		
