b3b61e46062082138f5df8be34f282ba32840f30
decai <decai@DESKTOP-UA2BMST>
2024-09-21 22:21:25 -0500

    Creacion clases en view (MenuAreolinea, Pantalla Inicio) Modificacion de ViewFacade Controller ActionListener AplMain
    Merge branch 'main' of https://github.com/JuanDavidG31/Proyecto1.git
    into main

---
+++ PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/view/PantallaInicio.java
@@ -0,0 +1,70 @@
+package co.edu.unbosque.view;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class PantallaInicio extends JFrame{
+	
+	private JButton btnCliente, btnAereolinea;
+	private JLabel lblFondo;
+	
+	public PantallaInicio() {
+		ventana();
+		labels();
+		botones();
+	}
+	
+	public void ventana() {
+		setBounds(50, 50, 700, 560);
+		setTitle("Aeropuerto el Dorado");
+        setDefaultCloseOperation(EXIT_ON_CLOSE); 
+        setResizable(false);
+        setLayout(null);
+	}
+	
+	public void botones() {
+		ImageIcon btnClienteImg = new ImageIcon("src/co/edu/unbosque/view/btnCliente.png");
+		btnCliente = new JButton(btnClienteImg);
+		btnCliente.setBounds(140, 250, 418, 30);
+		add(btnCliente);
+		
+		ImageIcon btnAereolineaImg = new ImageIcon("src/co/edu/unbosque/view/btnAerolinea.png");
+		btnAereolinea = new JButton(btnAereolineaImg);
+		btnAereolinea.setBounds(140, 320, 418, 30);
+		add(btnAereolinea);
+	}
+	
+	public void labels() {
+		lblFondo = new JLabel(new ImageIcon("src/co/edu/unbosque/view/pantallaInicioDorado.png"));
+		setContentPane(lblFondo);
+	}
+
+	public JButton getBtnCliente() {
+		return btnCliente;
+	}
+
+	public void setBtnCliente(JButton btnCliente) {
+		this.btnCliente = btnCliente;
+	}
+
+	public JButton getBtnAereolinea() {
+		return btnAereolinea;
+	}
+
+	public void setBtnAereolinea(JButton btnAereolinea) {
+		this.btnAereolinea = btnAereolinea;
+	}
+
+	public JLabel getLblFondo() {
+		return lblFondo;
+	}
+
+	public void setLblFondo(JLabel lblFondo) {
+		this.lblFondo = lblFondo;
+	}
+	
+	
+
+}
--- PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/view/ViewFacade.java
+++ PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/view/ViewFacade.java
@@ -1,5 +1,33 @@
 package co.edu.unbosque.view;
 
 public class ViewFacade {
+	
+	PantallaInicio pi;
+	MenuAereolinea ma;
+	
+	public ViewFacade() {
+		pi = new PantallaInicio();
+		ma = new MenuAereolinea();
+	}
+
+	public PantallaInicio getPi() {
+		return pi;
+	}
+
+	public void setPi(PantallaInicio pi) {
+		this.pi = pi;
+	}
+
+	public MenuAereolinea getMa() {
+		return ma;
+	}
+
+	public void setMa(MenuAereolinea ma) {
+		this.ma = ma;
+	}
+	
+	
+	
+	
 
 }
