19a851daef487c4451da1408a930958b6387c638
decai <decai@DESKTOP-UA2BMST>
2024-09-23 23:23:27 -0500

    Cambios minimos en el Controller - adicionar actionListener de botones View - textfield colocados, labels colocados, combobox colocados Creacion de nuevas variables combobox, textfield, labels

--- PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/controller/Controller.java
+++ PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/controller/Controller.java
@@ -37,6 +37,19 @@ public class Controller implements ActionListener {
 
 		vf.getMa().getAerolinea().addActionListener(this);
 		vf.getMa().getAerolinea().setActionCommand("seleccionarAerolinea");
+		
+		vf.getMa().getBtnAniadir().addActionListener(this);
+		vf.getMa().getBtnAniadir().setActionCommand("btnAniadir");
+		
+		vf.getMa().getBtnActualizar().addActionListener(this);
+		vf.getMa().getBtnActualizar().setActionCommand("btnActualizar");
+
+		vf.getMa().getBtnBuscarIzq().addActionListener(this);
+		vf.getMa().getBtnBuscarIzq().setActionCommand("btnBuscar");
+		
+		vf.getMa().getBtnSalir().addActionListener(this);
+		vf.getMa().getBtnSalir().setActionCommand("btnSalir");
+		 
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -56,81 +69,84 @@ public class Controller implements ActionListener {
 			if (vf.getMa().getAerolinea().getSelectedItem().equals("Avianca")) {
 
 				ImageIcon avianca = new ImageIcon("Images\\Avianca.png");
-				Image resizedA = avianca.getImage().getScaledInstance(100, 65, Image.SCALE_REPLICATE);
+				Image resizedA = avianca.getImage().getScaledInstance(100, 75, Image.SCALE_REPLICATE);
 				vf.getMa().getLogo().setIcon(new ImageIcon(resizedA));
 				aerolinea="Avianca";
 				
 			} else if (vf.getMa().getAerolinea().getSelectedItem().equals("Latam")) {
 
 				ImageIcon latam = new ImageIcon("Images\\Latam.png");
-				Image resizedL = latam.getImage().getScaledInstance(100, 50, Image.SCALE_REPLICATE);
+				Image resizedL = latam.getImage().getScaledInstance(100, 75, Image.SCALE_REPLICATE);
 				vf.getMa().getLogo().setIcon(new ImageIcon(resizedL));
 				aerolinea="Latam";
 				
 			} else if (vf.getMa().getAerolinea().getSelectedItem().equals("Wingo")) {
 
 				ImageIcon wingo = new ImageIcon("Images\\Wingo.png");
-				Image resizedW = wingo.getImage().getScaledInstance(100, 50, Image.SCALE_REPLICATE);
+				Image resizedW = wingo.getImage().getScaledInstance(100, 75, Image.SCALE_REPLICATE);
 				vf.getMa().getLogo().setIcon(new ImageIcon(resizedW));
 				aerolinea="Wingo";
 				
 			} else if (vf.getMa().getAerolinea().getSelectedItem().equals("Clic")) {
 
 				ImageIcon clic = new ImageIcon("Images\\Clic.png");
-				Image resizedC = clic.getImage().getScaledInstance(100, 50, Image.SCALE_REPLICATE);
+				Image resizedC = clic.getImage().getScaledInstance(100, 75, Image.SCALE_REPLICATE);
 				vf.getMa().getLogo().setIcon(new ImageIcon(resizedC));
 				aerolinea="Clic";
 				
 			} else if (vf.getMa().getAerolinea().getSelectedItem().equals("JetSmart")) {
 
 				ImageIcon jetSmart = new ImageIcon("Images\\jetsmart.png");
-				Image resizedJ = jetSmart.getImage().getScaledInstance(100, 50, Image.SCALE_REPLICATE);
+				Image resizedJ = jetSmart.getImage().getScaledInstance(100, 75, Image.SCALE_REPLICATE);
 				vf.getMa().getLogo().setIcon(new ImageIcon(resizedJ));
 				aerolinea="Jet Smart";
 				
 			} else if (vf.getMa().getAerolinea().getSelectedItem().equals("Satena")) {
 
 				ImageIcon satena = new ImageIcon("Images\\Satena.png");
-				Image resizedS = satena.getImage().getScaledInstance(100, 50, Image.SCALE_REPLICATE);
+				Image resizedS = satena.getImage().getScaledInstance(100, 75, Image.SCALE_REPLICATE);
 				vf.getMa().getLogo().setIcon(new ImageIcon(resizedS));
 				aerolinea="Satena";
 				
 			} else if (vf.getMa().getAerolinea().getSelectedItem().equals("Air Canada")) {
 
 				ImageIcon airCanada = new ImageIcon("Images\\AirCanada.png");
-				Image resizedAC = airCanada.getImage().getScaledInstance(100, 50, Image.SCALE_REPLICATE);
+				Image resizedAC = airCanada.getImage().getScaledInstance(100, 75, Image.SCALE_REPLICATE);
 				vf.getMa().getLogo().setIcon(new ImageIcon(resizedAC));
 				aerolinea="Air Canada";
 				
 			} else if (vf.getMa().getAerolinea().getSelectedItem().equals("KLM")) {
 
 				ImageIcon klm = new ImageIcon("Images\\Klm.png");
-				Image resizedK = klm.getImage().getScaledInstance(100, 50, Image.SCALE_REPLICATE);
+				Image resizedK = klm.getImage().getScaledInstance(100, 75, Image.SCALE_REPLICATE);
 				vf.getMa().getLogo().setIcon(new ImageIcon(resizedK));
 				aerolinea="KLM";
 				
 			} else if (vf.getMa().getAerolinea().getSelectedItem().equals("Iberia")) {
 
 				ImageIcon iberia = new ImageIcon("Images\\Iberia.png");
-				Image resizedI = iberia.getImage().getScaledInstance(100, 50, Image.SCALE_REPLICATE);
+				Image resizedI = iberia.getImage().getScaledInstance(100, 75, Image.SCALE_REPLICATE);
 				vf.getMa().getLogo().setIcon(new ImageIcon(resizedI));
 				aerolinea="Iberia";
 				
 			} else if (vf.getMa().getAerolinea().getSelectedItem().equals("Emirates")) {
 
 				ImageIcon emirates = new ImageIcon("Images\\Emirates.png");
-				Image resizedE = emirates.getImage().getScaledInstance(100, 50, Image.SCALE_REPLICATE);
+				Image resizedE = emirates.getImage().getScaledInstance(100, 75, Image.SCALE_REPLICATE);
 				vf.getMa().getLogo().setIcon(new ImageIcon(resizedE));
 				aerolinea="Emirates";
 				
 			} else if (vf.getMa().getAerolinea().getSelectedItem().equals("Delta")) {
 
 				ImageIcon delta = new ImageIcon("Images\\Delta.png");
-				Image resizedD = delta.getImage().getScaledInstance(100, 50, Image.SCALE_REPLICATE);
+				Image resizedD = delta.getImage().getScaledInstance(100, 75, Image.SCALE_REPLICATE);
 				vf.getMa().getLogo().setIcon(new ImageIcon(resizedD));
 				aerolinea="Delta";
 			}
 			
+			break;
+		case "btnSalir":
+			System.exit(0);
 			break;
 
 		default:
