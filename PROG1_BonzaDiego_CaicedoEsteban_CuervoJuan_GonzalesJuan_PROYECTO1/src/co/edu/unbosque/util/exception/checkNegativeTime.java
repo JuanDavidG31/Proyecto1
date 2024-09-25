5960e4f0e29cc8621621d5861f5e35bafdcabc70
juanp <juanp@172.17.36.213>
2024-09-25 11:44:35 -0500

    Ostia checkNegativeTime Javadoc

--- PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/util/exception/checkNegativeTime.java
+++ PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/util/exception/checkNegativeTime.java
@@ -1,10 +1,26 @@
 package co.edu.unbosque.util.exception;
 
-public class CheckNegativeTime extends Exception{
-	private static final long serialVersionUID = 1L;
+/**
+ * Excepción que se lanza cuando se intenta establecer un tiempo negativo.
+ * 
+ * <p>
+ * Esta clase extiende {@link Exception} y proporciona un mensaje de error 
+ * predeterminado que indica que la hora no puede ser negativa.
+ * </p>
+ */
+public class CheckNegativeTime extends Exception {
+    private static final long serialVersionUID = 1L;
 
-	public CheckNegativeTime() {
-		super("La hora no puede ser negativa");
-	}
-
+    /**
+     * Constructor por defecto de la excepción CheckNegativeTime.
+     * 
+     * <p>
+     * Este constructor llama al constructor de la superclase 
+     * {@link Exception} con un mensaje predeterminado que indica 
+     * que la hora no puede ser negativa.
+     * </p>
+     */
+    public CheckNegativeTime() {
+        super("La hora no puede ser negativa");
+    }
 }
