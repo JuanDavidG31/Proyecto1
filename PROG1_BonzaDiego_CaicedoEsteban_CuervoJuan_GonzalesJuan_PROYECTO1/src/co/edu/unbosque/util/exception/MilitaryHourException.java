0e9085da04400c30df5def80d0081bcf7f4d138d
juanp <juanp@172.17.36.213>
2024-09-25 11:44:10 -0500

    Javadoc Nuevas Excepciones

--- PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/util/exception/MilitaryHourException.java
+++ PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/util/exception/MilitaryHourException.java
@@ -1,7 +1,25 @@
 package co.edu.unbosque.util.exception;
 
+/**
+ * Excepción que se lanza cuando se proporciona una hora que no está en formato militar.
+ * 
+ * <p>
+ * Esta clase extiende {@link Exception} y proporciona un mensaje de error 
+ * predeterminado que indica que la hora debe estar en formato militar (2400).
+ * </p>
+ */
 public class MilitaryHourException extends Exception {
-	public MilitaryHourException() {
-		super("La hora debe estar en formato militar 2400"); // TODO Auto-generated constructor stub
-	}
+    
+    /**
+     * Constructor por defecto de la excepción MilitaryHourException.
+     * 
+     * <p>
+     * Este constructor llama al constructor de la superclase 
+     * {@link Exception} con un mensaje predeterminado que indica 
+     * que la hora debe estar en formato militar 2400.
+     * </p>
+     */
+    public MilitaryHourException() {
+        super("La hora debe estar en formato militar 2400");
+    }
 }
--- PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/util/exception/PassengersNegativeNumberException.java
+++ PROG1_BonzaDiego_CaicedoEsteban_CuervoJuan_GonzalesJuan_PROYECTO1/src/co/edu/unbosque/util/exception/PassengersNegativeNumberException.java
@@ -1,7 +1,26 @@
 package co.edu.unbosque.util.exception;
 
+/**
+ * Excepción que se lanza cuando se intenta establecer un número negativo de pasajeros.
+ * 
+ * <p>
+ * Esta clase extiende {@link Exception} y proporciona un mensaje de error 
+ * predeterminado que indica que el número de pasajeros no puede ser negativo.
+ * </p>
+ * 
+ */
 public class PassengersNegativeNumberException extends Exception {
-public PassengersNegativeNumberException() {
-super("El numero de pasajeros no puede ser negativo");	// TODO Auto-generated constructor stub
-}
+    
+    /**
+     * Constructor por defecto de la excepción PassengersNegativeNumberException.
+     * 
+     * <p>
+     * Este constructor llama al constructor de la superclase 
+     * {@link Exception} con un mensaje predeterminado que indica 
+     * que el número de pasajeros no puede ser negativo.
+     * </p>
+     */
+    public PassengersNegativeNumberException() {
+        super("El número de pasajeros no puede ser negativo");
+    }
 }
