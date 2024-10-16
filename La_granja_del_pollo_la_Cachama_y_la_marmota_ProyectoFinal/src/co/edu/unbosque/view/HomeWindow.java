1cc4953026973032df1646cd9168348cec3b5d24
juanp <juanp@LAPTOP-EUELHU61>
2024-10-15 20:31:11 -0500

    View 1

---
+++ La_granja_del_pollo_la_Cachama_y_la_marmota_ProyectoFinal/src/co/edu/unbosque/view/HomeWindow.java
@@ -0,0 +1,9 @@
+package co.edu.unbosque.view;
+
+import javax.swing.JFrame;
+
+public class HomeWindow extends JFrame {
+	public HomeWindow() {
+		// TODO Auto-generated constructor stub
+	}
+}
--- La_granja_del_pollo_la_Cachama_y_la_marmota_ProyectoFinal/src/co/edu/unbosque/view/ViewFacade.java
+++ La_granja_del_pollo_la_Cachama_y_la_marmota_ProyectoFinal/src/co/edu/unbosque/view/ViewFacade.java
@@ -1,5 +1,48 @@
 package co.edu.unbosque.view;
 
 public class ViewFacade {
+	private HomeWindow home;
+	private Schedule schedule;
+	private WorkShifts shifts;
+	private Treatments treatemnets;
 
+	public ViewFacade() {
+		home= new HomeWindow();
+		schedule= new Schedule();
+		shifts= new WorkShifts();
+		treatemnets= new Treatments();
+	}
+
+	public HomeWindow getHome() {
+		return home;
+	}
+
+	public void setHome(HomeWindow home) {
+		this.home = home;
+	}
+
+	public Schedule getSchedule() {
+		return schedule;
+	}
+
+	public void setSchedule(Schedule schedule) {
+		this.schedule = schedule;
+	}
+
+	public WorkShifts getShifts() {
+		return shifts;
+	}
+
+	public void setShifts(WorkShifts shifts) {
+		this.shifts = shifts;
+	}
+
+	public Treatments getTreatemnets() {
+		return treatemnets;
+	}
+
+	public void setTreatemnets(Treatments treatemnets) {
+		this.treatemnets = treatemnets;
+	}
+	
 }
