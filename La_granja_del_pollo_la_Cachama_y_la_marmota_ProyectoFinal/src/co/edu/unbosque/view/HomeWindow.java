fc32d4f99f515afa589d840bdf4be5b1c5e8bb4e
juanp <juanp@LAPTOP-EUELHU61>
2024-10-16 21:19:47 -0500

    Vista 1.1

--- La_granja_del_pollo_la_Cachama_y_la_marmota_ProyectoFinal/src/co/edu/unbosque/view/HomeWindow.java
+++ La_granja_del_pollo_la_Cachama_y_la_marmota_ProyectoFinal/src/co/edu/unbosque/view/HomeWindow.java
@@ -1,9 +1,55 @@
 package co.edu.unbosque.view;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class HomeWindow extends JFrame {
+	private JPanel mainPanel;
+
 	public HomeWindow() {
-		// TODO Auto-generated constructor stub
+		window();
+		panels();
+		inputs();
+		headlines();
+		buttons();
+
+		add(mainPanel);
+	}
+
+	public void window() {
+		setBounds(0, 0, 800, 600); // Window position and size (where it starts in x, where it starts in y,
+										// wide,high)
+		setTitle(""); // tittle on window
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // Close window and close the software
+		setResizable(false);
+		setLayout(null);
+
+	}
+
+	public void panels() {
+		mainPanel = new JPanel();
+		mainPanel.setBounds(0, 0, 800, 600);
+		mainPanel.setLayout(null);
+		
+	}
+	public void inputs() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	public void buttons() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void textFields() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void headlines() {
+		// TODO Auto-generated method stub
+
 	}
 }
