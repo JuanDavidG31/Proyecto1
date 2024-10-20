29ef218084867d52aceb29c77f477e261145c1cb
juanp <juanp@LAPTOP-EUELHU61>
2024-10-20 17:17:48 -0500

    agendar

--- La_granja_del_pollo_la_Cachama_y_la_marmota_ProyectoFinal/src/co/edu/unbosque/view/Schedule.java
+++ La_granja_del_pollo_la_Cachama_y_la_marmota_ProyectoFinal/src/co/edu/unbosque/view/Schedule.java
@@ -1,5 +1,6 @@
 package co.edu.unbosque.view;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,6 +10,7 @@ public class Schedule extends JFrame {
 	private JPanel mainPanel;
 	private JTextField name, email;
 	private JComboBox<String> doctor, specialty;
+	private JButton generate;
 
 	public Schedule() {
 		window();
@@ -30,9 +32,27 @@ public class Schedule extends JFrame {
 
 	private void comboBox() {
 		doctor = new JComboBox<String>();
+		doctor.setBounds(100, 100, 150, 100);
+		doctor.addItem("medico");
+		doctor.addItem("nombre 1");
+		doctor.addItem("nombre 2");
+		doctor.addItem("nombre 3");
+		doctor.addItem("nombre 4");
+		doctor.addItem("nombre 5");
+		doctor.addItem("nombre 6");
+		doctor.addItem("nombre 7");
 		mainPanel.add(doctor);
 
 		specialty = new JComboBox<String>();
+		specialty.setBounds(200, 200, 150, 100);
+		specialty.addItem("especialidad");
+		specialty.addItem("1");
+		specialty.addItem("2");
+		specialty.addItem("3");
+		specialty.addItem("4");
+		specialty.addItem("5");
+		specialty.addItem("6");
+		specialty.addItem("7");
 		mainPanel.add(specialty);
 
 	}
@@ -48,12 +68,20 @@ public class Schedule extends JFrame {
 	}
 
 	public void inputs() {
-		// TODO Auto-generated method stub
+		name = new JTextField("nombre");
+		name.setBounds(300, 300, 150, 50);
+		mainPanel.add(name);
+
+		email = new JTextField("correo electronico");
+		email.setBounds(400, 400, 150, 50);
+		mainPanel.add(email);
 
 	}
 
 	public void buttons() {
-		// TODO Auto-generated method stub
+		generate= new JButton("Agendar");
+		generate.setBounds(500, 500, 100, 50);
+		mainPanel.add(generate);
 
 	}
 
