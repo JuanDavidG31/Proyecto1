28d4e4fa463ab17c020d6b8866ebec03a4ff176b
juanp <juanp@LAPTOP-EUELHU61>
2024-11-09 17:49:35 -0500

    sha

--- La_granja_del_pollo_la_Cachama_y_la_marmota_ProyectoFinal/src/co/edu/unbosque/controller/Controller.java
+++ La_granja_del_pollo_la_Cachama_y_la_marmota_ProyectoFinal/src/co/edu/unbosque/controller/Controller.java
@@ -3777,6 +3777,8 @@ public class Controller implements ActionListener {
 									if (mf.getDoctor().update(new DoctorDTO(null, null, id, null, null), new DoctorDTO(
 											dc.getName(), dc.getEmail(), id, dc.getSpecialty(), "inactivo"))) {
 										// correo de la persona 1
+										
+										
 									}
 
 								}
@@ -4070,7 +4072,6 @@ public class Controller implements ActionListener {
 	}
 
 	public void emailTurn(String email, String name, int id, String speciality, String date1, String date2) {
-		JOptionPane.showMessageDialog(null, "Paciente creado con exito");
 
 		Properties prop = FileHandler.loadProperties("mail.properties");
 
@@ -4083,6 +4084,10 @@ public class Controller implements ActionListener {
 				.replace("{fechaFin}", date2);
 
 		sendEmail(email, subject, message);
+	}
+	
+	public void exchangeTurn1(String email, String name1,String name2, int id, String speciality ) {
+		
 	}
 
 	public static void eliminarArchivos(String folderName, String fileName) {
